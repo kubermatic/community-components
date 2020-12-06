2014e01079404bd3a85cb627e7050ff5b8adb294
Tobias Schneck <tobias.schneck@loodse.com>
2020-12-06 23:39:56 +0100

    update latest deployment scripts, to helm 3

--- helper/kubermatic-installer-script/kubermatic-deploy.sh
+++ helper/kubermatic-installer-script/kubermatic-deploy.sh
@@ -1,169 +1,182 @@
 #!/usr/bin/env bash
-
-if [ "$#" -lt 1 ] || [ "${1}" == "--help" ]; then
-  cat <<EOF
-Usage: $(basename $0) (master|seed) path/to/VALUES_FILE  path/to/CHART_FOLDER "[EXTRA HELM ARGUMENTS]"
-EOF
-  exit 0
-fi
+# Kubermatic Deployment script based on helm3
 set -euo pipefail
 
-if [[ ! -f ${2} ]]; then
-    echo "VALUES_FILE not found! $2"
-    exit 1
-fi
-if [[ ! -d ${3} ]]; then
-    echo "CHART_FOLDER not found! $3"
-    exit 1
+if [[ $# -lt 1 ]] || [[ "$1" == "--help" ]]; then
+  echo "Usage: $(basename \"$0\") (master|seed) path/to/VALUES_FILES path/to/CHART_FOLDER (monitoring|logging|backup|kubermatic|kubermatic-deployment-only)"
+  echo "FYI: kubermatic|kubermatic-deployment-only is deprecated due to new kubermatic-installer binary"
+  exit 1
 fi
 
-HELM_EXTRA_ARGS=${@:4}
-VALUES_FILE=$(realpath ${2})
-CHART_FOLDER=$(realpath ${3})
-if [[ "${1}" = "master" ]]; then
+DEPLOY_TYPE="$1"
+if [[ "$DEPLOY_TYPE" = master ]]; then
   MASTER_FLAG="--set=kubermatic.isMaster=true"
-else
+elif [[ "$DEPLOY_TYPE" = seed ]]; then
   MASTER_FLAG="--set=kubermatic.isMaster=false"
+else
+  echo "invalid deploy type, expected (master|seed), got $DEPLOY_TYPE"
 fi
-DEPLOY_NODEPORT_PROXY=${DEPLOY_NODEPORT_PROXY:-true}
-DEPLOY_ALERTMANAGER=${DEPLOY_ALERTMANAGER:-true}
-DEPLOY_MINIO=${DEPLOY_MINIO:-true}
-DEPLOY_STACK=${DEPLOY_STACK:-kubermatic}
-TILLER_NAMESPACE=${TILLER_NAMESPACE:-kubermatic}
-HELM_INIT_ARGS=${HELM_INIT_ARGS:-""}
-SKIP_TILLER_INIT=${SKIP_TILLER_INIT:-true}
-#replaces var KUBERMATIC_TAG in master charts of https://github.com/kubermatic/kubermatic/tree/master/config
-KUBERMATIC_TAG=${KUBERMATIC_TAG:-""}
 
-source $(dirname "$0")/lib.sh
-[[ $(helm version -c --short) =~ ^Client.*v2.*$ ]] && echo "helm2 detected!" || (echo "This script requires helm2! Please install helm2: https://v2.helm.sh/docs/using_helm" && exit 1)
+VALUES_FILE=$(realpath "$2")
+if [[ ! -f "$VALUES_FILE" ]]; then
+    echo -e "$(date -Is)" "'values.yaml' in folder not found! \nCONTENT $VALUES_FILE:\n`ls -l $VALUES_FILE/..`"
+    exit 1
+fi
+CHART_FOLDER=$(realpath "$3")
+if [[ ! -d "$CHART_FOLDER" ]]; then
+    echo "$(date -Is)" "CHART_FOLDER not found! $CHART_FOLDER"
+    exit 1
+fi
+### verification is checked in case expresion
+DEPLOY_STACK="$4"
 
-function replace() {
-    if [ -z "$KUBERMATIC_TAG" ]; then
-        echo "skip replacement of tags -> no deployment of master branch"
-    else
-        echo "replace '__KUBERMATIC_TAG__' with '$KUBERMATIC_TAG'!"
-        sed -i "s/__KUBERMATIC_TAG__/${KUBERMATIC_TAG}/g" $CHART_FOLDER/kubermatic/Chart.yaml
-        sed -i "s/__KUBERMATIC_TAG__/${KUBERMATIC_TAG}/g" $CHART_FOLDER/kubermatic/values.yaml
-        sed -i "s/__KUBERMATIC_TAG__/${KUBERMATIC_TAG}/g" $CHART_FOLDER/kubermatic-operator/*
-    fi
-}
+DEPLOY_CERTMANAGER=true
+DEPLOY_MINIO=true
+DEPLOY_ALERTMANAGER=true
+DEPLOY_LOKI=true
+DEPLOY_ELASTIC=false
+DEPLOY_IAP=true
+#CANARY_DEPLOYMENT=true
+RESTART_POD_PRESETS=true
+PER_NAMESPACE_FOLDER=$(dirname $VALUES_FILE)/pernamespace
+
+#verify Helm3
+[[ $(helm version --short) =~ ^v3.*$ ]] && echo "helm3 detected!" || (echo "This script requires helm3! Please install helm3: https://helm.sh/docs/intro/install" && exit 1)
 
 function deploy {
-  local name=$1
-  local namespace=$2
+  local name="$1"
+  local namespace="$2"
   local path="$CHART_FOLDER/$3"
-  local timeout=${4:-300}
+  if [[ "${4:-false}" == "true" ]] && [[ -d $PER_NAMESPACE_FOLDER ]]; then
+    local deploy_per_ns=true
+  fi
+  local timeout="${5:-300s}"
 
-  if [[ ! -d $path ]]; then
+  if [[ ! -d "$path" ]]; then
     echo "chart not found! $path"
     exit 1
   fi
-  TEST_NAME="[Helm] Deploy chart ${name}"
+  TEST_NAME="[Helm] Deploy chart $name"
 
-  inital_revision="$(retry 5 helm list --tiller-namespace=${TILLER_NAMESPACE} ${name} --output=json|jq '.Releases[0].Revision')"
-
-  echodate "Upgrading ${name}..."
-  retry 5 helm --tiller-namespace ${TILLER_NAMESPACE} upgrade --install --atomic --timeout $timeout ${MASTER_FLAG} ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace ${namespace} ${name} ${path}
-
-  if [ "${CANARY_DEPLOYMENT:-}" = "true" ]; then
-    TEST_NAME="[Helm] Rollback chart ${name}"
-    echodate "Rolling back ${name} to revision ${inital_revision} as this was only a canary deployment"
-    retry 5 helm --tiller-namespace ${TILLER_NAMESPACE} rollback --wait --timeout $timeout ${name} ${inital_revision}
+  if [[ -v CANARY_DEPLOYMENT ]]; then
+    inital_revision="$(helm history $name --output=json | jq '.Releases[0].Revision')"
   fi
 
+  echo "$(date -Is)" "Upgrading $namespace ..."
+  kubectl create namespace "$namespace" || true
+  if [[ -v deploy_per_ns ]]; then
+    kubectl apply -n "$namespace" -f ${PER_NAMESPACE_FOLDER}/pernamespace
+  fi
+
+  helm upgrade --install --wait --timeout $timeout $MASTER_FLAG --values "$VALUES_FILE" --namespace "$namespace" "$name" "$path"
+
+  if [[ -v CANARY_DEPLOYMENT ]]; then
+    TEST_NAME="[Helm] Rollback chart $name"
+    echo "$(date -Is)" "Rolling back $name to revision $inital_revision as this was only a canary deployment"
+    helm rollback --wait --timeout "$timeout" "$name" "$inital_revision"
+  fi
+
+  if [[ -v deploy_per_ns ]] && [[ -v RESTART_POD_PRESETS ]]; then
+      echo "$(date -Is)" "Restart deployment, daemonsets in: $namespace..."
+      kubectl  -n $namespace get deploy -o name | xargs -L1 kubectl -n $namespace rollout restart || echo "no deployment found!"
+      kubectl  -n $namespace get ds -o name | xargs -L1 kubectl -n $namespace  rollout restart || echo "no daemonset found!"
+  fi
   unset TEST_NAME
 }
 
-function initTiller() {
-    if [[ "${SKIP_TILLER_INIT}" = true ]]; then
-        echo "[Helm] skipp Tiller init"
-    else
-      TEST_NAME="[Helm] Init Tiller"
-      echodate "Initializing Tiller in namespace ${TILLER_NAMESPACE}"
-      helm version --client
-      kubectl create serviceaccount -n ${TILLER_NAMESPACE} tiller-sa --dry-run -oyaml|kubectl apply -f -
-      kubectl create clusterrolebinding tiller-cluster-role --clusterrole=cluster-admin --serviceaccount=${TILLER_NAMESPACE}:tiller-sa  --dry-run -oyaml|kubectl apply -f -
-      retry 5 helm --tiller-namespace ${TILLER_NAMESPACE} init --service-account tiller-sa --replicas 3 --history-max 10 --upgrade --force-upgrade --wait ${HELM_INIT_ARGS}
-      echodate "Tiller initialized successfully"
-      unset TEST_NAME
-  fi
+function deployKubermaticOperator() {
+    echo "$(date -Is)" "Deploying the CRD's..."
+    kubectl apply -f "$CHART_FOLDER/kubermatic/crd/"
+    if [[ "$DEPLOY_TYPE" == master ]]; then
+      deploy kubermatic-operator kubermatic kubermatic-operator/ true
+
+    echo "$(date -Is)" "Apply the Kubermatic config files..."
+    #skip .yaml files with where no apiVersion is specified like the values.yaml
+    grep -l 'apiVersion' $(dirname $VALUES_FILE)/*.yaml | xargs -L1 kubectl apply -f
+    fi
 }
 
-replace
-echodate "Deploying ${DEPLOY_STACK} stack..."
-case "${DEPLOY_STACK}" in
-  monitoring)
-    initTiller
-    deploy "node-exporter" "monitoring" ./monitoring/node-exporter/
-    deploy "kube-state-metrics" "monitoring" ./monitoring/kube-state-metrics/
-    deploy "grafana" "monitoring" ./monitoring/grafana/
-    deploy "blackbox-exporter" "monitoring" ./monitoring/blackbox-exporter/
-    if [[ "${DEPLOY_ALERTMANAGER}" = true ]]; then
-      deploy "alertmanager" "monitoring" ./monitoring/alertmanager/
-#      since kubermatic version 2.13
-#      if [[ "${1}" = "master" ]]; then
-#        deploy "karma" "monitoring" ./monitoring/karma/
-#      fi
+function deployKubermaticCharts() {
+    echo "$(date -Is)" "Deploying the CRD's..."
+    kubectl apply -f "$CHART_FOLDER/kubermatic/crd/"
+    if [[ "$DEPLOY_TYPE" == master ]]; then
+      deploy kubermatic kubermatic kubermatic/
     fi
+}
 
-    # Prometheus can take a long time to become ready, depending on the WAL size.
-    # We try to accomodate by waiting for 15 instead of 5 minutes.
-    deploy "prometheus" "monitoring" ./monitoring/prometheus/ 900
+function deployBackup() {
+      # CI has its own Minio deployment as a proxy for GCS, so we do not install the default Helm chart here.
+    if [[ -v DEPLOY_MINIO ]]; then
+      deploy    minio minio minio/
+      deploy    s3-exporter kube-system s3-exporter/
+    fi
+}
+
+function deployIAP() {
+    # We might have not configured IAP which results in nothing being deployed. This triggers https://github.com/helm/helm/issues/4295 and marks this as failed
+    # We hack around this by grepping for a string that is mandatory in the values file of IAP
+    # to determine if its configured, because am empty chart leads to Helm doing weird things
+    if [[ -v DEPLOY_IAP ]]; then
+      ### not used until iap ca field is not in chart
+      if grep -q oidc_issuer_url "$VALUES_FILE"; then
+        deploy iap iap iap/
+      else
+        echo "$(date -Is)" "Skipping IAP deployment because discovery_url is unset in values file"
+      fi
+    fi
+}
+
+
+echo "$(date -Is)" "Deploying $DEPLOY_STACK stack..."
+case "$DEPLOY_STACK" in
+  monitoring)
+    deployIAP
+    deploy      node-exporter monitoring monitoring/node-exporter/
+    deploy      kube-state-metrics monitoring monitoring/kube-state-metrics/
+    deploy      grafana monitoring monitoring/grafana/
+    deploy      blackbox-exporter monitoring monitoring/blackbox-exporter/
+    if [[ -v DEPLOY_ALERTMANAGER ]]; then
+      deploy    alertmanager monitoring monitoring/alertmanager/
+    fi
+    deploy      prometheus monitoring monitoring/prometheus/ 900s
     ;;
 
   logging)
-    initTiller
-    deploy "elasticsearch" "logging" ./logging/elasticsearch/
-    deploy "fluentbit" "logging" ./logging/fluentbit/
-    deploy "kibana" "logging" ./logging/kibana/
-    deploy "loki" "logging" ./logging/loki/
-    deploy "promtail" "logging" ./logging/promtail/
+    #### deprecated -> Loki
+    if [[ "${DEPLOY_ELASTIC}" = true ]]; then
+      deploy "elasticsearch" "logging" logging/elasticsearch/
+      deploy "fluentbit" "logging" logging/fluentbit/
+      deploy "kibana" "logging" logging/kibana/
+    fi
+    if [[ "${DEPLOY_LOKI}" = true ]]; then
+      deploy "loki" "logging" logging/loki/
+      deploy "promtail" "logging" logging/promtail/
+    fi
     ;;
 
   kubermatic)
-    initTiller
+    if [[ "$DEPLOY_TYPE" == master ]]; then
+      deploy nginx-ingress-controller nginx-ingress-controller nginx-ingress-controller/
 
-    echodate "Deploying the CRD's..."
-    retry 5 kubectl apply -f $CHART_FOLDER/kubermatic/crd/
-
-    if [[ "${1}" = "master" ]]; then
-      deploy "nginx-ingress-controller" "nginx-ingress-controller" ./nginx-ingress-controller/
-      deploy "cert-manager" "cert-manager" ./cert-manager/
-      deploy "certs" "default" ./certs/
-      deploy "oauth" "oauth" ./oauth/
-      # We might have not configured IAP which results in nothing being deployed. This triggers https://github.com/helm/helm/issues/4295 and marks this as failed
-      # We hack around this by grepping for a string that is mandatory in the values file of IAP
-      # to determine if its configured, because am empty chart leads to Helm doing weird things
-      if grep -q 'discovery_url\|oidc_issuer_url' ${VALUES_FILE}; then
-        deploy "iap" "iap" ./iap/
-      else
-        echodate "Skipping IAP deployment because discovery_url is unset in values file"
+      if [[ -v DEPLOY_CERTMANAGER ]]; then
+        #### CERT-MANAGER
+        kubectl apply -f "$CHART_FOLDER/cert-manager/crd"
+        deploy    cert-manager cert-manager cert-manager/
       fi
+      deploy    oauth oauth oauth/
     fi
-
-    # CI has its own Minio deployment as a proxy for GCS, so we do not install the default Helm chart here.
-    if [[ "${DEPLOY_MINIO}" = true ]]; then
-      deploy "minio" "minio" ./minio/
-      deploy "s3-exporter" "kube-system" ./s3-exporter/
-    fi
-
-    # The NodePort proxy is only relevant in cloud environments (Where LB services can be used)
-    if [[ "${DEPLOY_NODEPORT_PROXY}" = true ]]; then
-      deploy "nodeport-proxy" "nodeport-proxy" ./nodeport-proxy/
-    fi
-
-    # Kubermatic
-    deploy "kubermatic" "kubermatic" ./kubermatic/
+    deployBackup
+    deployKubermaticOperator
     ;;
 
   kubermatic-deployment-only)
-    # Kubermatic only without other components
-    deploy "kubermatic" "kubermatic" ./kubermatic/
+    deployKubermaticOperator
     ;;
 
-  kubermatic-operator)
-    kubectl create namespace kubermatic-operator || true
-    kubectl apply -f ./kubermatic-operator/
+  backup)
+    deployBackup
     ;;
+
+  *)
+    echo "error: no DEPLOY_STACK defined"
 esac
