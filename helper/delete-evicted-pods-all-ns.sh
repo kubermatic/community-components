8f19d0a001313e7fcfd7f372c9b8a6638295a9a1
Tobias Schneck <tobias.schneck@loodse.com>
2021-05-16 17:01:27 +0200

    fix delete evicted pod script

--- helper/delete-evicted-pods-all-ns.sh
+++ helper/delete-evicted-pods-all-ns.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
-set -x
+flag=${1:-"--no-delete"}
+set -euo pipefail
 
 kubectl get ns --no-headers | awk '{print $1}' > ns-list
 
 while read -r ns; do
-   kubectl get pods -o jsonpath='{.items[?(@.status.reason=="Evicted")].metadata.name}' -n $ns
+   evicted_pods=$(kubectl get pods -o jsonpath='{.items[?(@.status.reason=="Evicted")].metadata.name}' -n $ns)
+   for pod in $evicted_pods; do
+    echo "$pod"
+    if [[ "$flag" == "--delete" ]]; then
+      kubectl delete pod $pod -n $ns
+    fi
+   done
 done < ns-list
 rm ns-list
